3ac3f554a95a4a2647b238430f9d5a0bb4140fdb
Md. Shahraj Mashrafe Mashfe <shahrajmashfe@gmail.com>
2025-11-13 22:04:51 +0600

    Add functionality for call button and update coin deduction logic

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+// funcion to get innet Text
+
+
 // function to get inner text(int)
 function getInnerText(id){
     const element = parseInt(document.getElementById(id).innerText);
@@ -20,5 +23,29 @@ for (let heart of hearts) {
     const availableHearts = getInnerText("total-hearts");
     const totalHearts = availableHearts + 1;
     setInnerText("total-hearts", totalHearts);
+  });
+}
+
+// call button functionality
+const callBtns = document.getElementsByClassName("call-btn");
+
+for(let callBtn of callBtns){
+  callBtn.addEventListener("click", function(e){
+    e.preventDefault();
+    const card = callBtn.closest(".card");
+    const serviceName = card.querySelector(".service-name").innerText;
+    const serviceNum = card.querySelector(".service-num").innerText;
+
+    const availableCoins = getInnerText("total-coins");
+    if(availableCoins < 20){
+      alert("Insufficient coin!")
+    }
+    else
+    {
+      console.log(serviceName)
+      alert("Calling...\n" + serviceName +"\n"+ serviceNum);
+      const totalCoins = availableCoins - 20;
+      setInnerText("total-coins", totalCoins);
+    }
   });
 }
